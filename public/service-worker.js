ee795fe3664cf31422b3de5fa72f7181633b3484
Lyman Connor <eckzly@gmail.com>
2022-01-03 19:28:17 -0500

    added service-worker file and folder for js files

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/database.js",
+    "./js/index.js",
+    "./",
+];
