04d4795d003cdc49b4ffb0599e44e635e67b5992
Lyman Connor <eckzly@gmail.com>
2022-01-16 20:11:18 -0500

    corrected id names and populate functions for offline transactions

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let transactions = [];
 let myChart;
 
@@ -23,16 +25,115 @@ function populateTotal() {
 }
 
 function populateTable() {
-    let tableBody = document.querySelector("#tableBody");
-    tableBody.innerHTML = "";
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
 
     transactions.forEach((transaction) => {
-        let offTr = document.createElement("offTr");
-        offTr.innerHTML = `
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
             <td>${transaction.name}</td>
             <td>${transaction.value}</td>
             `;
 
-        tableBody.appendChild(offTr);
+        tbody.appendChild(tr);
     });
 }
+
+function populateChart() {
+    let reverse = transactions.slice().reverse();
+    let sum = 0;
+
+    let labels = reverse.map((t) => {
+        let date = new Date(t.date);
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    });
+
+    let data = reverse.map((t) => {
+        sum += parseInt(t.value);
+        return sum;
+    });
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    let context = document.getElementById("myChart").getContext("2d");
+
+    myChart = new Chart(context, {
+        type: "line",
+        data: {
+            labels,
+            datasets: [
+                {
+                    label: "Total Over Time",
+                    fill: true,
+                    backgroundColor: "#7777ff",
+                    data,
+                },
+            ],
+        },
+    });
+}
+
+function sendTransaction(isAdding) {
+    let nameEl = document.querySelector("#t-name");
+    let amountEl = document.querySelector("#t-amount");
+    let errorEl = document.querySelector(".form .error");
+
+    if (nameEl.value === "" || amountEl.value === "") {
+        errorEl.textContent = "Information is missing.";
+        return;
+    } else {
+        errorEl.textContent = "";
+    }
+
+    let transaction = {
+        name: nameEl.value,
+        value: amountEl.value,
+        date: new Date().toISOString(),
+    };
+
+    if (!isAdding) {
+        transaction.value *= -1;
+    }
+
+    transactions.unshift(transactions);
+
+    populateChart();
+    populateTable();
+    populateTotal();
+
+    fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(transaction),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+        },
+    })
+        .then((reponse) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.errors) {
+                errorEl.textContent = "Information is missing";
+            } else {
+                nameEl.value = "";
+                amountEl.value = "";
+            }
+        })
+        .catch((err) => {
+            saveRecord(transaction);
+
+            nameEl.value = "";
+            amountEl.value = "";
+        });
+}
+
+document.querySelector("#add-btn").onclick = function () {
+    sendTransaction(true);
+};
+
+document.querySelector("#sub-btn").onclick = function () {
+    sendTransaction(false);
+};
